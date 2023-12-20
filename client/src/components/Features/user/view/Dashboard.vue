baf3000582aa1d2b29db648b4d1947e606de14f5
Thibaud BASSET <basset.thibaud@gmail.com>
2023-12-20 17:21:17 +0100

    Update Dashboard.vue

--- client/src/components/Features/user/view/Dashboard.vue
+++ client/src/components/Features/user/view/Dashboard.vue
@@ -100,45 +100,60 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="container p-10 w-full">
-    <h1 class="text-primary text-2xl font-bold text-center">Bienvenue sur le Dashboard ! </h1>
-    <h1 class="text-secondary text-2xl font-bold text-center">Voici quelques statistiques </h1>
+  <div class="drawer lg:drawer-open">
+    <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content flex flex-col items-center">
+      <div class="container p-10 w-full">
+        <h1 class="text-primary text-2xl font-bold text-center">Bienvenue sur le Dashboard ! </h1>
+        <h1 class="text-secondary text-2xl font-bold text-center">Voici quelques statistiques </h1>
 
-    <div class="stats stats-vertical lg:stats-horizontal shadow">
+        <div class="stats stats-vertical lg:stats-horizontal shadow mb-5">
 
-      <div class="stat">
-        <div class="stat-title">Meubilaires</div>
-        <div class="stat-value">{{ meubilaires.length }}</div>
-      </div>
+          <div class="stat">
+            <div class="stat-title">Meubilaires</div>
+            <div class="stat-value">{{ meubilaires.length }}</div>
+          </div>
 
-      <div class="stat">
-        <div class="stat-title">Matériaux</div>
-        <div class="stat-value">{{ materials.length }}</div>
-      </div>
+          <div class="stat">
+            <div class="stat-title">Matériaux</div>
+            <div class="stat-value">{{ materials.length }}</div>
+          </div>
 
-      <div class="stat">
-        <div class="stat-title">Catégories</div>
-        <div class="stat-value">{{ categories.length }}</div>
+          <div class="stat">
+            <div class="stat-title">Catégories</div>
+            <div class="stat-value">{{ categories.length }}</div>
+          </div>
+
+        </div>
+
+        <div id="stats" class="md:flex gap-20">
+          <div class="card w-96 bg-base-100 shadow-xl">
+            <div class="card-body">
+              <div class="card-title">Meubilaires par catégories</div>
+              <canvas id="meubilairesByCategoryChart"></canvas>
+            </div>
+          </div>
+          <div class="card w-96 bg-base-100 shadow-xl">
+            <div class="card-body">
+              <div class="card-title">Meubilaires par matériaux</div>
+              <canvas id="meubilairesByMaterialChart"></canvas>
+            </div>
+          </div>
+        </div>
+
       </div>
 
     </div>
-
-    <div id="stats" class="md:flex gap-20">
-      <div class="card w-96 bg-base-100 shadow-xl">
-        <div class="card-body">
-          <div class="card-title">Meubilaires par catégories</div>
-          <canvas id="meubilairesByCategoryChart"></canvas>
-        </div>
-      </div>
-      <div class="card w-96 bg-base-100 shadow-xl">
-        <div class="card-body">
-          <div class="card-title">Meubilaires par matériaux</div>
-          <canvas id="meubilairesByMaterialChart"></canvas>
-        </div>
-      </div>
+    <div class="drawer-side">
+      <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
+      <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content gap-4 font-bold">
+        <!-- Sidebar content here -->
+        <li><router-link to="/">Accueil</router-link></li>
+        <li><router-link to="/logout">Déconnexion</router-link></li>
+      </ul>
     </div>
-
   </div>
+
 
 </template>
 
