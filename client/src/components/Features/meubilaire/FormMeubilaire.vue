99ca981a3e4ff42b764767e046e9b6c87609adc9
Alexandre LUSIAK <alexandre.lusiak@gmail.com>
2023-12-20 16:22:47 +0100

    formulaire d'ajout

---
+++ client/src/components/Features/meubilaire/FormMeubilaire.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="container">
+        <h1>Ajouter un meuble</h1>
+        <form>
+            <div class="form-input">
+                <input type="text" placeholder="Nom" class="input input-bordered w-full max-w-xs" />
+
+            </div>
+
+            <div class="form-input">
+                <textarea class="textarea textarea-bordered w-80 h-64" placeholder="Bio"  ></textarea>
+            </div>
+            <div class="form-input">
+                <label>noyer</label>
+                <input type="radio" name="radio-1" class="radio m-1" checked />
+                <label>Aluminium</label>
+                <input type="radio" placeholder="Aluminium" name="radio-1" class="radio " />
+            </div>
+             
+            <input type="file" class="file-input file-input-bordered w-full max-w-xs" />
+        </form>
+    </div>
+</template>
+
+<style scoped>
+
+.container {
+    text-align: center;
+    padding: 2rem;
+    border: 2px solid black;
+    margin: auto;
+}
+.form-input {
+    margin: 1rem;
+}
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/Home.vue';
 import Login from '../components/Features/user/view/Login.vue';
 import Dashboard from "../components/Features/user/view/Dashboard.vue";
+import FormMeubilaire from '../components/Features/meubilaire/FormMeubilaire.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -9,6 +10,7 @@ const router = createRouter({
     {path: '/', component: Home},
     {path: '/login', component: Login},
     {path: '/dashboard', component: Dashboard},
+    {path: '/add-meubilaire', component: FormMeubilaire},
   ],
 })
 
